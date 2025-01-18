31d80ca0cada7af18eb9e17b74b9defce5f873e5
Ángel Díaz <ad9311@proton.me>
2025-01-18 16:43:30 -0500

    Implement create expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -9,11 +9,21 @@ class ExpensesController < ApplicationController
   end
 
   def create
+    @expense = budget.expenses.new(expense_params)
+    if @expense.save
+      redirect_to budget_path(budget.uid), notice: "Expense created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def budget
     @budget ||= Budget.find_by(uid: params[:budget_uid])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:description, :amount, :expense_category_id)
   end
 end
