85b20d33d7d61c94a8b6c624cc04b94a1c0c5b35
Ángel Díaz <ad9311@proton.me>
2025-01-19 16:28:55 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 expense_categories = [
-  { uid: "food", name: "Food" },
-  { uid: "drink", name: "Drink" },
+  { uid: "banking", name: "Banking" },
+  { uid: "clothing", name: "Clothing" },
   { uid: "entertainment", name: "Entertainment" },
-  { uid: "transport", name: "Transport" },
-  { uid: "other", name: "Other" }
+  { uid: "extra", name: "Extra" },
+  { uid: "foodDelivery", name: "Food Delivery" },
+  { uid: "groceries", name: "Groceries" },
+  { uid: "home", name: "Home" },
+  { uid: "insurance", name: "Insurance" },
+  { uid: "onlineShopping", name: "Online Shopping" },
+  { uid: "other", name: "Other" },
+  { uid: "restaurants", name: "Restaurants" },
+  { uid: "savings", name: "Savings" },
+  { uid: "subscriptions", name: "Subscriptions" },
+  { uid: "transportation", name: "Transportation" },
+  { uid: "utilities", name: "Utilities" },
+  { uid: "wages", name: "Wages" }
 ]
 expense_categories.each do |params|
   ExpenseCategory.find_or_create_by!(params)
