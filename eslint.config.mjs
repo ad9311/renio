0c54223c351d3861b8476d653216027a46bcbef3
Ángel Díaz <ad9311@proton.me>
2025-01-20 20:28:44 -0500

    Add stylelint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
-  { ignores: ["app/assets/builds", "bun.config.js", "tailwind.config.js"] },
+  { ignores: ["app/assets/builds/**/*.js", "bun.config.js", "tailwind.config.js"] },
   pluginJs.configs.recommended,
 ];
---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-tailwindcss/scss",
+    "stylelint-config-prettier-scss",
+  ],
+  plugins: [],
+  rules: {},
+  ignoreFiles: ["app/assets/builds/**/*.css"],
+};
