30f26cfe3a89fb71949ba06ef95859bd2d9ef8ee
Ángel Díaz <ad9311@proton.me>
2025-01-30 22:13:15 -0500

    Remove back button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   allow_browser versions: :modern
-  before_action :track_navigation_history
-
-  private
-
-  def track_navigation_history
-    return unless user_signed_in?
-
-    session[:navigation_history] ||= []
-    session[:navigation_history] << request.path if request.path != session[:navigation_history].last
-    session[:navigation_history].shift if session[:navigation_history].size > 10
-  end
+  before_action :authenticate_user!
 end
