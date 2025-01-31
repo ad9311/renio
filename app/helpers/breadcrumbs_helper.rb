e34bed8fbf7229e5de1f91d53c28dc07c258b847
Ángel Díaz <ad9311@proton.me>
2025-01-31 13:02:26 -0500

    WIP breadcrumbs for account receivables

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -7,6 +7,8 @@ module BreadcrumbsHelper
     case controller
     when :budgets
       breadcrumbs.concat breadcrumbs_for_budgets(action)
+    when :account_receivables
+      breadcrumbs.concat breadcrumbs_for_account_receivables(action)
     else
       breadcrumbs
     end
@@ -20,10 +22,24 @@ module BreadcrumbsHelper
       [{ name: 'Budgets' }]
     when :show
       [{ name: 'Budget' }]
-    when :new
-      [{ name: 'New budget' }]
     when :edit
-      [{ name: 'Edit budget' }]
+      [{ name: 'Budget', path: budget_path(@budget) }, { name: 'Edit' }]
+    else
+      []
+    end
+  end
+
+  def breadcrumbs_for_account_receivables(action)
+    case action
+    when :index
+      [{ name: 'Account receivables' }]
+    when :show
+      [{ name: 'Account receivable' }]
+    when :new
+      [{ name: 'Account Receivables', path: account_receivables_path }, { name: 'New' }]
+    when :edit
+      show_path = { name: 'Account receivable', path: account_receivable_path(@account_receivable) }
+      [show_path, { name: 'Edit' }]
     else
       []
     end
