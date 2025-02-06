10dd1de138b790754df3c7da97a98ffdcc1756ae
Ángel Díaz <ad9311@proton.me>
2025-02-05 20:27:04 -0500

    Implement breadcrumbs for receivables, payments and expenses

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -9,6 +9,12 @@ module BreadcrumbsHelper
       breadcrumbs.concat breadcrumbs_for_budgets(action)
     when :account_receivables
       breadcrumbs.concat breadcrumbs_for_account_receivables(action)
+    when :receivables
+      breadcrumbs.concat breadcrumbs_for_receivables(action)
+    when :payments
+      breadcrumbs.concat breadcrumbs_for_payments(action)
+    when :expenses
+      breadcrumbs.concat breadcrumbs_for_expenses(action)
     else
       breadcrumbs
     end
@@ -29,6 +35,22 @@ module BreadcrumbsHelper
     end
   end
 
+  def breadcrumbs_for_expenses(action)
+    budget = { name: 'Budget', path: budget_path(@budget) }
+
+    case action
+    when :new
+      [budget, { name: 'New expense' }]
+    when :show
+      [budget, { name: 'Expense' }]
+    when :edit
+      expense = { name: 'Expense', path: budget_expense_path(@budget, @expense) }
+      [budget, expense, { name: 'Edit' }]
+    else
+      []
+    end
+  end
+
   def breadcrumbs_for_account_receivables(action)
     case action
     when :index
@@ -40,6 +62,38 @@ module BreadcrumbsHelper
     when :edit
       show_path = { name: 'Account receivable', path: account_receivable_path(@account_receivable) }
       [show_path, { name: 'Edit' }]
+    else
+      []
+    end
+  end
+
+  def breadcrumbs_for_receivables(action)
+    account_receivable = { name: 'Account receivable', path: account_receivable_path(@account_receivable) }
+
+    case action
+    when :new
+      [account_receivable, { name: 'New receivable' }]
+    when :show
+      [account_receivable, { name: 'Receivable' }]
+    when :edit
+      receivable = { name: 'Receivable', path: account_receivable_receivable_path(@account_receivable, @receivable) }
+      [account_receivable, receivable, { name: 'Edit' }]
+    else
+      []
+    end
+  end
+
+  def breadcrumbs_for_payments(action)
+    account_receivable = { name: 'Account receivable', path: account_receivable_path(@account_receivable) }
+
+    case action
+    when :new
+      [account_receivable, { name: 'New payment' }]
+    when :show
+      [account_receivable, { name: 'Payment' }]
+    when :edit
+      payment = { name: 'Payment', path: account_receivable_payment_path(@account_receivable, @payment) }
+      [account_receivable, payment, { name: 'Edit' }]
     else
       []
     end
